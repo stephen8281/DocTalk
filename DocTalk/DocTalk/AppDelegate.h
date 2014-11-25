c4229304cec0eaabd030a59014b50f4798ab603f
Capstone Group 65 <Group65@pocahontas.staff.ece.ubc.ca>
2014-11-25 15:16:43 -0800

    Started building the user interface

---
+++ DocTalk/DocTalk/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DocTalk
+//
+//  Created by Capstone Group 65 on 2014-11-25.
+//  Copyright (c) 2014 Capstone Group 65. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DocTalk/DocTalk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DocTalk
+//
+//  Created by Capstone Group 65 on 2014-11-25.
+//  Copyright (c) 2014 Capstone Group 65. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
