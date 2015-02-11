05afd78a70c380e0f6ef5d0459fdc5b58456deb8
Stephen <stephen8281@hotmail.com>
2015-02-10 20:26:50 -0800

    moved the JSQ library into docTalk folder

--- DocTalk/LoginViewController.m
+++ DocTalk/LoginViewController.m
@@ -50,8 +50,9 @@
         } else {
             NSString *post =[[NSString alloc] initWithFormat:@"username=%@&password=%@",[self.txtUsername text],[self.txtPassword text]];
             NSLog(@"PostData: %@",post);
-            
-            NSURL *url=[NSURL URLWithString:@"http://localhost/jsonlogin2.php"];
+      
+            NSURL *url=[NSURL URLWithString:@"http://192.168.1.73/jsonlogin2.php"];
+            //NSURL *url=[NSURL URLWithString:@"http://localhost/jsonlogin2.php"];
             
             NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
             
